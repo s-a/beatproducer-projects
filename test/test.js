dbb92591dc0f85472728cb26be4b131830714779
s-a <stephan.ahlf@googlemail.com>
2015-03-17 13:27:24 +0100

    added tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,34 @@
 var fs = require("fs");
 var path = require("path");
+var https = require('https');
 var should = require("should");
+var shell = require('shelljs');
+
+var getRepoFiles = function (done) {
+	var options = {
+	  host: 'api.github.com',
+	  path: "/repos/s-a/beatproducer-projects/contents/projects",
+	  method: 'GET',
+      headers: {'user-agent': 'node.js'}
+	};
+
+	var callback = function(response) {
+	  var str = '';
+
+	  //another chunk of data has been recieved, so append it to `str`
+	  response.on('data', function (chunk) {
+	    str += chunk;
+	  });
+
+	  //the whole response has been recieved, so we just print it out here
+	  response.on('end', function () {
+
+	    done(JSON.parse(str));
+	  });
+	};
+	https.get(options, callback).end();
+};
+
 
 function getFiles (dir, files_){
     files_ = files_ || [];
@@ -10,19 +38,41 @@ function getFiles (dir, files_){
         if (fs.statSync(name).isDirectory()){
             getFiles(name, files_);
         } else {
-        	if (path.extname(name).toLowerCase() === ".json" ){
+ //       	if (path.extname(name).toLowerCase() === ".json" ){
         		files_.push(name);
-        	}
+//        	}
         }
     }
     return files_;
 }
 
+function getFileOwner(filename){
+	var res = filename.split(" ");
+	res.pop();
+	return res.join(" ");
+}
 
 var files = getFiles(path.join(__dirname, "..", "projects"));
-	console.log(files);
+var giturl,currentGitHubUser;
+
+before(function  () {
+	var s = shell.exec("git remote -v");
+	giturl = s.output.split("\n")[0].split("(fetch)")[0].split("origin")[1].trim();
+	console.log("remote:", giturl);
+	currentGitHubUser = giturl.replace("https://github.com/", "").split("/")[0];
+	console.log("user:", currentGitHubUser);
+});
 
 describe("validation", function() {
+	this.timeout(15000);
+
+
+	it("should contain only .json files", function() {
+		for (var i = 0; i < files.length; i++) {
+			var file = files[i];
+			path.extname(file).toLowerCase().should.be.equal(".json");
+		}
+	});
 
 	it("should contain only valid project .json files", function() {
 		for (var i = 0; i < files.length; i++) {
@@ -30,4 +80,22 @@ describe("validation", function() {
 			require(file).bpm.should.be.above(0);
 		}
 	});
+
+	it("should got git repo infos", function() {
+		currentGitHubUser.length.should.be.above(1);
+	});
+
+	it("should not delete files from other users", function(done) {
+		getRepoFiles(function(contents) {
+			for (var i = 0; i < contents.length; i++) {
+				var file = contents[i];
+				if ( ! fs.existsSync( path.join("./../", file.path) ) ){
+					console.warn("checking deleted file", file.path);
+					getFileOwner(file.name).should.be.equal(currentGitHubUser);
+				}
+			}
+			done();
+		});
+	});
+
 });
